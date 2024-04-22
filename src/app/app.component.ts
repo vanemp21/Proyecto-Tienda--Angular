a8d3eb96b7711379838495ff206046bfda06c755
Vanessa <vanemp21@gmail.com>
2024-04-22 19:13:59 +0200

    feat(add): Added firsts components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ListProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
